f7a2e336ab0a89bbacae1d0a0c8ddf23d025c5b5
hey-Zayn <zaynobusiness@gmail.com>
2026-03-29 12:33:33 +0500

    test: added new testing

--- backend/vitest.config.js
+++ backend/vitest.config.js
@@ -5,7 +5,10 @@ export default defineConfig({
     test: {
         globals: true, // Globals like describe, it, expect are available everywhere
         setupFiles: ['./__tests__/setup.db.js'], // A global hook to start the memory server
-        hookTimeout: 30000, // MongoDB memory server can take some time to download/spin up
-        testTimeout: 30000,
+        hookTimeout: 300000, // MongoDB memory server can take some time to download/spin up
+        testTimeout: 60000,
+        threads: {
+            singleThread: true,
+        },
     },
 });
